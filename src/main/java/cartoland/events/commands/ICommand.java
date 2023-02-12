fcb2c49a5962709001426723a3c291d84a98f5fa
Mango <evan940415@gmail.com>
2023-02-12 18:13:28 +0800

    remove: useless file

--- src/main/java/cartoland/events/commands/ICommand.java
+++
@@ -1,8 +0,0 @@
-package cartoland.events.commands;
-
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-
-public interface ICommand
-{
-    void commandProcess(SlashCommandInteractionEvent event); //指令
-}
